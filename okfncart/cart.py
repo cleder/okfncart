e7a9ecce2693e09d41b6620d833c939fde032d95
Christian Ledermann <christian.ledermann@gmail.com>
2014-08-31 19:17:16 +0100

    begin refactor: offers implemented as object

--- okfncart/cart.py
+++ okfncart/cart.py
@@ -5,10 +5,14 @@ from . import getproducts
 
 class Cart(object):
 
-    def __init__(self, products):
+    def __init__(self, products, offers=None):
         self._contents = {}
         self._products = products
         self._discounts = {}
+        if not offers:
+            self._offers=[]
+        else:
+            self._offers= offers
 
     def load_products(self, filename):
         self._products = getproducts.loadproducts(filename)
@@ -31,6 +35,9 @@ class Cart(object):
         price = 0.0
         for content in self.contents.items():
             price += self._products[content[0]] * content[1]
+        self._discounts = {}
+        for offer in self._offers:
+            offer.add_discount(self)
         for discount in self._discounts.items():
             price += discount[1]
         return price
--- okfncart/offers.py
+++ okfncart/offers.py
@@ -2,33 +2,59 @@
 # Copyright (C) 2014  Christian Ledermann
 #
 
-def product_bogof(cart, product):
+
+class Offer(object):
+
+    def __init__(self):
+        raise NotImplementedError
+
+    def add_discount(self, cart):
+        raise NotImplementedError
+
+class OfferBogof(Offer):
     '''
     "buy one get one free" on product
     '''
-    if product in cart.contents:
-        price = cart._products[product]
-        num = cart.contents[product]/2
-        cart._add_discount('%s bogo' % product, -1 * num * price)
+    def __init__(self, product):
+        self._product = product
+
+    def add_discount(self, cart):
+        product = self._product
+        if product in cart.contents:
+            price = cart._products[product]
+            num = cart.contents[product]/2
+            cart._add_discount('%s bogo' % product, -1 * num * price)
 
 
-def product_b2g3rdf(cart, product):
+class OfferB2g3rdf(Offer):
     '''
-    buy of product of strawberries, and get the third free
+    buy two of product and get the third free
     '''
-    if product in cart.contents:
-        price = cart._products[product]
-        num = cart.contents[product]/3
-        cart._add_discount('%s b2g3rd' % product, -1 * num * price)
+    def __init__(self, product):
+        self._product = product
 
-def p1_p2_off(cart, product1, product2, p2_discount):
+    def add_discount(self, cart):
+        product = self._product
+        if product in cart.contents:
+            price = cart._products[product]
+            num = cart.contents[product]/3
+            cart._add_discount('%s b2g3rd' % product, -1 * num * price)
+
+
+class OfferP1p2XpcOff(Offer):
     '''
     get p2_discount (*100%) off product2 if you buy a product1 at the same time
     '''
-    if product1 in cart.contents and product2 in cart.contents:
-        price = cart._products[product2] * p2_discount
-        num = min(cart.contents[product1], cart.contents[product2])
-        discount = int(p2_discount * 100)
-        cart._add_discount('%i pc discount for  %s if you buy %s ' %
-                            (discount, product2, product1),
-                            -1 * num * price)
+    def __init__(self, product1, product2, p2_discount):
+        self._product1 = product1
+        self._product2 = product2
+        self._p2_discount = p2_discount
+
+    def add_discount(self, cart):
+        if self._product1 in cart.contents and self._product2 in cart.contents:
+            price = cart._products[self._product2] * self._p2_discount
+            num = min(cart.contents[self._product1], cart.contents[self._product2])
+            discount = int(self._p2_discount * 100)
+            cart._add_discount('%i pc discount for  %s if you buy %s ' %
+                                (discount, self._product2, self._product1),
+                                -1 * num * price)
